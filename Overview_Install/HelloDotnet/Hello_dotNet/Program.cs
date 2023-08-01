d5acff4e99cb7e540905e7b806879052a398e5f6
Chris Bridgewater <cgbridgewater@outlook.com>
2023-07-31 17:10:42 -0700

    first commit

---
+++ Overview_Install/HelloDotnet/Hello_dotNet/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string place = "Coding Dojo";
+
+Console.WriteLine($"hello {place}");
+
+Console.WriteLine("hello {0}", place);
+
+int temperature = 65;
+
+if(temperature >= 72){
+    Console.WriteLine("It's a great day to make hay!");
+} else {
+    Console.WriteLine("Time to binge some Netflix!");
+}
