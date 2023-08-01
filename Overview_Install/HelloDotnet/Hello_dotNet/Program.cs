ed6b1c84d5a1dd050c51ba53bad535112b5a736f
Chris Bridgewater <cgbridgewater@outlook.com>
2023-07-31 17:24:40 -0700

    pulling out to create conditionals page

--- Overview_Install/HelloDotnet/Hello_dotNet/Program.cs
+++ Overview_Install/HelloDotnet/Hello_dotNet/Program.cs
@@ -7,10 +7,3 @@ Console.WriteLine($"hello {place}");
 
 Console.WriteLine("hello {0}", place);
 
-int temperature = 65;
-
-if(temperature >= 72){
-    Console.WriteLine("It's a great day to make hay!");
-} else {
-    Console.WriteLine("Time to binge some Netflix!");
-}
