de692165ce738956de406b02321b385c8093cac5
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-02 11:55:14 -0700

    first commit

---
+++ Language_Fundamentals/OOP_With_C_Sharp/Class_and_Object_Construction/Dog.cs
@@ -0,0 +1,37 @@
+//// a class allows you to group several parameters or ideas into one object
+
+class Dog  // class structure  ALWAYS capitalized!!!!
+{
+    private string Name;  // <= fields, keep capitalized
+    public string _Name   // <= getter
+    { 
+        get { return Name; }
+        set { if(value[0] != 'H') Name = value; }
+    }
+    string Breed;
+    string FurColor {get; set;}
+    
+    // Notice how this is like a function taking in a parameter for name
+    public Dog(string name, string breed, string fur){ // <= constructor call on the class and include variables assoacted to the field
+        // Notice how we used a capital "N" for our Name above, but a lower case "n" for our parameter
+        // This helps us to distinguish which is which
+        Name = name;
+        Breed = breed;
+        FurColor = fur;
+    }
+
+    void Bark(){  // <= methods
+        Console.WriteLine("Bark! Bark!");
+    }
+    // void Fetch(string item){
+    //     Console.WriteLine($"{Name} hurriers to fetch the {Item}!");
+    // }
+
+}
+
+ // Notice the type here is "Dog". Classes create their very own data type! 
+ // PLEASE NOTE: This line will throw an error for now until we make
+ // the proper adjustments on the next page! The syntax IS correct!
+// Dog MyDog = new Dog("Juniper");
+
+
--- Language_Fundamentals/OOP_With_C_Sharp/Class_and_Object_Construction/Program.cs
+++ Language_Fundamentals/OOP_With_C_Sharp/Class_and_Object_Construction/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dog Juniper = new Dog("Juniper", "Mini Aussie", "White with spots");
+
+Console.WriteLine(Juniper._Name);
+
+Juniper._Name = "Harley";
+Console.WriteLine(Juniper._Name);
+Juniper._Name = "Gooby";
+
+Console.WriteLine(Juniper._Name);
