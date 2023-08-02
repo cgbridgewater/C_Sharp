9d5f29b77ddb67cf5ab379a8ca4b0c508c3299ec
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-01 18:56:06 -0700

    finish assignment

--- Language_Fundamentals/Fundamentals_III/Program.cs
+++ Language_Fundamentals/Fundamentals_III/Program.cs
@@ -91,3 +91,71 @@ int[] TestIntArray = new int[] {-1,2,3,-4,5};
 // You should get back [0,2,3,0,5], think about how you will show that this worked
 // NonNegatives(TestIntArray);
 Console.WriteLine(string.Join(",", NonNegatives(TestIntArray)));
+
+
+
+// 6. Print Dictionary
+// Given a dictionary, print the contents of the said dictionary.
+
+static void PrintDictionary(Dictionary<string,string> MyDictionary)
+{
+    foreach(KeyValuePair<string,string> attribute in MyDictionary){
+        Console.WriteLine($"{attribute.Key} - {attribute.Value}");
+    }
+}
+Dictionary<string,string> TestDict = new Dictionary<string,string>();
+TestDict.Add("HeroName", "Iron Man");
+TestDict.Add("RealName", "Tony Stark");
+TestDict.Add("Powers", "Money and intelligence");
+PrintDictionary(TestDict);
+
+
+
+// 7. Find Key
+// Given a search term, return true or false whether the given term is a key in a dictionary. (Hint: figuring this one out may require some research!)
+
+static bool FindKey(Dictionary<string,string> MyDictionary, string SearchTerm)
+{
+    foreach(KeyValuePair<string,string> attribute in MyDictionary){
+        if (attribute.Key == SearchTerm){
+            return true;
+        }
+    }
+    return false;
+}
+// Use the TestDict from the earlier example or make your own
+// This should print true
+Console.WriteLine(FindKey(TestDict, "RealName"));
+// This should print false
+Console.WriteLine(FindKey(TestDict, "Name"));
+// This should print true
+Console.WriteLine(FindKey(TestDict, "HeroName"));
+
+
+
+// 8. Generate a Dictionary
+// Given a List of names and a List of integers, create a dictionary where the key is a name from the List of names and the value is a number from the List of numbers. Assume that the two Lists will be of the same length. Don't forget to print your results to make sure it worked.
+
+// Ex: Given ["Julie", "Harold", "James", "Monica"] and [6,12,7,10], return a dictionary
+// {
+//	"Julie": 6,
+//	"Harold": 12,
+//	"James": 7,
+//	"Monica": 10
+// } 
+static Dictionary<string,int> GenerateDictionary(List<string> Names, List<int> Numbers)
+{
+    Dictionary<string,int> NamesAndNumbers = new Dictionary<string, int>();
+    for(int idx = 0; idx < Names.Count; idx++){
+        NamesAndNumbers.Add(Names[idx], Numbers[idx]);
+        // Console.WriteLine($"{Names[idx]} + {Numbers[idx]}");
+    }
+    foreach(KeyValuePair<string,int> result in NamesAndNumbers){
+        Console.WriteLine($"Name - {result.Key}: Number - {result.Value}");
+    }
+    return NamesAndNumbers;
+}
+// We've shown several examples of how to set your tests up properly, it's your turn to set it up!
+// Your test code here
+GenerateDictionary(new List<string> (new string[]{"Julie", "Harold", "James", "Monica"}) , new List<int>(new int[]{6,12,7,10}));
+// List<string> nameList = new List<string> (new string[]{"steve", "mike", "tim"});
