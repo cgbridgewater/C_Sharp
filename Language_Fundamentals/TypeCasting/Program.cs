58f4071a644a7aa9c5d74326f7ecab849341cd30
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-01 19:40:06 -0700

    lesson workings

--- Language_Fundamentals/TypeCasting/Program.cs
+++ Language_Fundamentals/TypeCasting/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//// Implicit Casting
+
+int IntegerValue = 3;
+double DoubleValue = IntegerValue;
+
+Console.WriteLine(DoubleValue);
+
+
+//// Explicit Casting
+
+double DoubleValue2 = 3.1459;
+int IntegerValue2 = (int)DoubleValue2; // Notice the (int) to cast it to integer before setting it to an int value
+
+Console.WriteLine(IntegerValue2);
+
+
+//// Unboxing  -- Not A Good Practice!!
+// object BoxedData = "This is clearly a string";
+object BoxedData = 2;
+Console.WriteLine("Whats in the boooooox....!?");
+// Make sure it is the type you need before proceeding
+if (BoxedData is int) {
+    // This shouldn't run
+    Console.WriteLine("I guess we have an integer value in this box?");
+}
+if (BoxedData is string) {
+    Console.WriteLine("It is totally a string in the box!");
+}
+
+
+// //// Safe Explicit Casting
+// // THIS WILL WORK!!
+// object ActuallyString = "a string";
+// string ExplicitString = ActuallyString as string;
+ 
+// // THIS WON'T WORK!!
+// object ActuallyInt = 256;
+// int ExplicitInt = ActuallyInt as int;
--- Language_Fundamentals/UserInput/Program.cs
+++ Language_Fundamentals/UserInput/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Type Something, then push enter: ");
+// string InputLine = Console.ReadLine();
+// Console.WriteLine($"You wrote: {InputLine}");
+
+
+////// Fails if not a number
+// Console.WriteLine("Type a number, then push enter: ");
+// string InputLine = Console.ReadLine();
+// int converted = Convert.ToInt32(InputLine);
+// Console.WriteLine($"You wrote: {converted}");
+////// Fails if not a number
+
+
+// This can catch string or number! 
+Console.WriteLine("Type a number, then hit enter");
+string NumberInput = Console.ReadLine();
+if(Int32.TryParse(NumberInput, out int j)){
+    Console.WriteLine($"The number was {j}");
+}
+else {
+    Console.WriteLine("I'll ask one more time, type a number, then hit enter");
+    string NumberInput2 = Console.ReadLine();
+    if(Int32.TryParse(NumberInput2, out int i)){
+        Console.WriteLine($"The number was {i}");
+    }else{
+        Console.WriteLine("Nevermind, Exiting....");
+    }
+}
