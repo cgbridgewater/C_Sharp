f974ae4bff979f8ce389fd0684c7879112a51a0b
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-03 10:45:40 -0700

    finish assignment

--- Language_Fundamentals/OOP_With_C_Sharp/Drink_Maker/Program.cs
+++ Language_Fundamentals/OOP_With_C_Sharp/Drink_Maker/Program.cs
@@ -2,8 +2,7 @@
 Coffee BlackCoffee = new Coffee("Black Coffee", "Black","Arabica", "Dark" , 100, 0, false);
 Soda MtDew = new Soda("Mt. Dew", "Yellow", false, 30, 180);
 Wine Merlot = new Wine("Merlot", "Red", 40, 122, "Napa Valley", 2019);
-// Coffee MyDrink = new Soda(); // // error CS7036: There is no argument given that corresponds to the required formal parameter 'name' of 'Soda.Soda(string, string, bool, double, int)'
-// Coffee MyDrink = new Soda("Coke", "Brown", false, 30, 190); // // Cannot implicitly convert type 'Soda' to 'Coffee'
+
 
 List<Drink> AllBeverages = new List<Drink>();
 AllBeverages.Add(BlackCoffee);
@@ -14,3 +13,11 @@ foreach(Drink d in AllBeverages)
 {
     d.ShowInfo();
 }
+
+
+
+
+
+// These are invalid because we cannot make instances across child classes
+// Coffee MyDrink = new Soda(); // // error CS7036: There is no argument given that corresponds to the required formal parameter 'name' of 'Soda.Soda(string, string, bool, double, int)'
+// Coffee MyDrink = new Soda("Coke", "Brown", false, 30, 190); // // Cannot implicitly convert type 'Soda' to 'Coffee'
