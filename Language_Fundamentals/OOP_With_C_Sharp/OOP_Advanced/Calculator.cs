0188cf54663e25c73bbedc20b70220ed37249097
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-04 13:58:57 -0700

    working with lesson material

---
+++ Language_Fundamentals/OOP_With_C_Sharp/OOP_Advanced/Calculator.cs
@@ -0,0 +1,19 @@
+public static class Calculator
+{
+    public static double Add(double FirstValue, double SecondValue)
+    {
+        return FirstValue + SecondValue;
+    }
+    public static double Subtract(double FirstValue, double SecondValue)
+    {
+        return FirstValue - SecondValue;
+    }
+    public static double Multiply(double FirstValue, double SecondValue)
+    {
+        return FirstValue * SecondValue;
+    }
+    public static double Square(double FirstValue)
+    {
+        return FirstValue * FirstValue;
+    }
+}
--- Language_Fundamentals/OOP_With_C_Sharp/OOP_Advanced/Program.cs
+++ Language_Fundamentals/OOP_With_C_Sharp/OOP_Advanced/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double FirstValue = 4.8;
+double SecondValue = 2;
+
+double Sum = Calculator.Add(FirstValue,SecondValue);
+Console.WriteLine($"{FirstValue} + {SecondValue} = {Sum}");
+
+double Subtract = Calculator.Subtract(FirstValue,SecondValue);
+Console.WriteLine($"{FirstValue} - {SecondValue} = {Subtract}");
+
+
+double Multiply = Calculator.Multiply(Sum,Subtract);
+Console.WriteLine($"{Sum} * {Subtract} = {Multiply}");
+
+double Square = Calculator.Square(Multiply);
+Console.WriteLine($"{Multiply} squared = {Square}");
