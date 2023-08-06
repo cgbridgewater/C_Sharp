b39e1489908c3050efddec6eff4e4851f8905a79
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-06 15:46:11 -0700

    lesson work

---
+++ ASP_NET_Core/ASP_MVC_II/ModelsAndForms/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ModelsAndForms.Models;
+
+namespace ModelsAndForms.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("register")]
+    public IActionResult ResultPage() // updated
+    {    
+        // process the form...
+        // You can start by logging the data to the console
+        // or using the debugger to inspect the HogwartsStudent instance!
+        // Console.WriteLine(student.Name);
+        // Console.WriteLine(student.House);
+        // Console.WriteLine(student.CurrentYear);
+        return View();
+    } 
+
+    public IActionResult Register(HogwartsStudent student)
+    {
+
+        return View(student);
+    }
+
+    public IActionResult HogwartsForm()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASP_NET_Core/ASP_MVC_II/ModelsAndForms/Models/HogwartsStudent.cs
@@ -0,0 +1,9 @@
+#pragma warning disable CS8618
+namespace ModelsAndForms.Models;
+
+public class HogwartsStudent
+{
+    public string Name {get;set;}
+    public string House {get;set;}
+    public int CurrentYear {get;set;}
+}
