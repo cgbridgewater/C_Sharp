884581508ad649ef9318313a48ca5c3cee07c13d
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-06 17:26:52 -0700

    completing assignment

---
+++ ASP_NET_Core/ASP_MVC_II/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers;
+
+public class HomeController : Controller
+{
+    static User user;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Survey()
+    {
+        return View();
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(User newUser)
+    {
+        user = newUser;
+        return RedirectToAction("Results");
+    }
+
+    [HttpGet("results")]
+    public IActionResult Results()
+    {
+        return View(user);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
