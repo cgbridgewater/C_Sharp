3f6c28e85efa8c320252e1728f383cba09f585a0
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-06 17:36:44 -0700

    fixing controller

--- ASP_NET_Core/ASP_MVC_II/DojoSurveyWithModel/Controllers/HomeController.cs
+++ ASP_NET_Core/ASP_MVC_II/DojoSurveyWithModel/Controllers/HomeController.cs
@@ -14,11 +14,13 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    [HttpGet("")]
     public IActionResult Index()
     {
         return View();
     }
 
+    [HttpGet("survey")]
     public IActionResult Survey()
     {
         return View();
