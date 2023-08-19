8951ebaba6240e2d243c9277ba5e65ca961eaa3a
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-19 15:18:56 -0700

    working with lambda functions

---
+++ ORMs/LambdaExpressions/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> numbers = new List<int>(){2,10,18,12,5,7,16};
+
+List<int> numsAboveTen = numbers.Where(num => num > 10).ToList();
+
+    Console.WriteLine("numbers above 10 :");
+foreach(int num in numsAboveTen){
+    Console.WriteLine(num);
+}
+
+Console.WriteLine("lowest number = " + numbers.Min(numbers => numbers));
+
+Console.WriteLine("highest number = " + numbers.Max(numbers => numbers));
