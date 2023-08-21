64d3cf30d5b2b422868e825641b6d0c50eabae9a
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-21 13:06:47 -0700

    first commit

---
+++ ORMs/TestProject/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TestProject.Models;
+
+namespace TestProject.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        // Now any time we want to access our database we use _context   
+        List<Monster> AllMonsters = _context.Monsters.ToList();
+        return View();    
+
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
