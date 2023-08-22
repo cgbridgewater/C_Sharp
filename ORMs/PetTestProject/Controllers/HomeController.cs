b177d3ee1282110c2a62058561ef6a2c8fc9b9a2
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-21 20:54:04 -0700

    working with CRUD features

--- ORMs/PetTestProject/Controllers/HomeController.cs
+++ ORMs/PetTestProject/Controllers/HomeController.cs
@@ -15,11 +15,82 @@ public class HomeController : Controller
         _context = context;
     }
 
-    public IActionResult Index()
+    [HttpGet("")]
+    public IActionResult Index() //View ALL
     {
-        List<Pet> AllPets = context.Monsters.ToList();
+        // ViewBag.AllPets = _context.Pets.ToList();
+        // ViewBag.AllDogs = _context.Pets.Where(d => d.PetType == "Dog");
+        // ViewBag.MostRecent = _context.Pets.OrderByDescending(m => m.CreatedAt).Take(5).ToList();
+        List<Pet> AllPets = _context.Pets.ToList();
+        return View(AllPets);
+    }
+
+    [HttpGet("pets/new")] //Create One Form Page
+    public IActionResult NewPet()
+    {
         return View();
     }
+
+
+    [HttpPost("pets/create")] // Create One Action Route
+    public IActionResult CreatePet(Pet newPet)
+    {
+        if(ModelState.IsValid)
+        {
+            _context.Add(newPet);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        } else {
+            return View("NewPet");
+        }
+    }
+
+    [HttpGet("pets/{id}")]
+    public IActionResult OnePet(int id) // View One
+    {
+        Pet? OnePet = _context.Pets.FirstOrDefault(i=> i.PetId == id);
+        return View(OnePet);
+    }
+
+    [HttpGet("pets/{Id}/edit")] // Get/populate update form
+    public IActionResult EditPet(int Id)
+    {
+            Pet? PetToEdit = _context.Pets.FirstOrDefault(a => a.PetId == Id); 
+                if(PetToEdit != null)
+                {
+                    return View(PetToEdit);
+                } else{
+                    return RedirectToAction("Index");
+                }
+    }
+
+    [HttpPost("pets/{Id}/update")] // Post action for Update
+    public IActionResult UpdatePet(int Id,Pet NewVersion)
+    {
+        Pet? OldVersion = _context.Pets.FirstOrDefault(p => p.PetId == Id);
+        if(ModelState.IsValid)
+        {
+            OldVersion.PetName = NewVersion.PetName;
+            OldVersion.PetType = NewVersion.PetType;
+            OldVersion.PetAge = NewVersion.PetAge;
+            OldVersion.HasFur = NewVersion.HasFur;
+            OldVersion.UpdatedAt = DateTime.Now;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        } else {
+            return View("EditPet", OldVersion);
+        }
+    }
+
+    [HttpPost("pets/{Id}/delete")] // Delete Action
+    public IActionResult DeletePet(int Id)
+    {
+        Pet? PetToDelete = _context.Pets.SingleOrDefault(p => p.PetId == Id);
+        _context.Pets.Remove(PetToDelete);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
 
     public IActionResult Privacy()
     {
--- ORMs/PetTestProject/Models/Pet.cs
+++ ORMs/PetTestProject/Models/Pet.cs
@@ -1,13 +1,25 @@
 #pragma warning disable CS8618
 using System.ComponentModel.DataAnnotations;
 namespace PetTestProject.Models;
+
 public class Pet
 {
     [Key]
     public int PetId {get; set;}
+    
+    [Required(ErrorMessage = "REQUIRED!")]   
+    [MinLength(2, ErrorMessage="Must be at least 2 characters in length.")]  
     public string PetName {get; set;}
+
+    [Required(ErrorMessage = "REQUIRED!")]   
+    [MinLength(2, ErrorMessage="Must be at least 2 characters in length.")]  
     public string PetType {get; set;}
+
+    [Required(ErrorMessage = "REQUIRED!")] 
+    [Range(1,int.MaxValue)]
     public int PetAge {get; set;}
+
+    [Required(ErrorMessage = "REQUIRED!")] 
     public bool HasFur {get; set;}
     public  DateTime CreatedAt {get; set;} = DateTime.Now;
     public  DateTime UpdatedAt {get; set;} = DateTime.Now;
