3253fa71787f39f184d13acb6de606e577a96c06
Chris Bridgewater <cgbridgewater@outlook.com>
2023-08-29 16:11:27 -0700

    touching up styple and wording

--- ORMs/ChefsNDishes/Models/Dish.cs
+++ ORMs/ChefsNDishes/Models/Dish.cs
@@ -8,18 +8,22 @@ public class Dish
     public int DishId { get; set; }
 
     [Required(ErrorMessage = "REQUIRED!")]   
-    [MinLength(2, ErrorMessage="Must be at least 2 characters in length.")]  
+    [MinLength(2, ErrorMessage="Must be at least 2 characters in length.")] 
+    [Display(Name = "Dish Name: ")] 
     public string DishName { get; set; } 
 
+    [Display(Name = "Tastiness: ")]
     public int Tastiness { get; set; }
 
     [Required(ErrorMessage = "REQUIRED!")]   
     [Range(0,5000000, ErrorMessage="Must be greater than 0.")]  
+    [Display(Name = "Calories: ")]
     public int? Calories { get; set; }
 
     public DateTime CreatedAt { get; set; } = DateTime.Now;
     public DateTime UpdatedAt { get; set; } = DateTime.Now;
 
+    [Display(Name = "Select Chef: ")]
     public int? UserId { get; set; }
     // Our navigation property to track which User made this Post
     // It is VERY important to include the ? on the datatype or this won't work!
